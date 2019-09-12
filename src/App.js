65bbe8e9a969749d0bacf548d71af8f32c525205
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-09-12 14:43:25 -0500

    score and top score works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './App.css'
 import Image from './components/Image.js'
 import allPokemon from './allPokemon.json'
 var shuffle = require('shuffle-array')
@@ -8,8 +9,8 @@ shuffle(allPokemon)
 
 export class App extends Component {
   state={
-    score: '',
-    topScore: '',
+    score: 0,
+    topScore: 0,
     clickedImages: [],
     allPokemon
   }
@@ -19,16 +20,42 @@ export class App extends Component {
       console.log(e.target)
       
       let newArr = this.state.clickedImages
+      
+      if (newArr.includes(e.target.id)){
+        alert("You Lost!")
+        this.setState({
+          clickedImages: []
+        })
+        if(this.state.topScore < this.state.score){
+          this.setState({
+            topScore: this.state.score
+          })
+        }
+
+        this.setState({
+          score: 0
+        })
+      }else{
+
+      this.setState({
+        score:  this.state.score + 1,
+      })
+
       newArr.push(e.target.id)
 
       this.setState({
         clickedImages: newArr
       })
 
+         
 
-      // shuffle(this.state.allPokemon)
 
-      // this.setState({allPokemon})
+
+
+    }
+
+    shuffle(this.state.allPokemon)
+    this.setState({allPokemon})
 
   
     }
